5ba6a8e3768a4ce89e6998f81071f2aea881182a
=Goza de Farinha <odamasenganumeko@gmail.com>
2023-09-10 16:30:03 -0300

    Estrutura base para o projeto, apenas com o esqueleto da página index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery livraria -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- JavaScript compilado-->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+    
+<?php
+
+    echo "Bem Vindo ao Site!!";
+
+?>
+
+</body>
+
+</html>
