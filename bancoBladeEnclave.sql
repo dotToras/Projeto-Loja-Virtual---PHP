71dc263256a25302bfdb5794eed7eff4821c0a8e
=Goza de Farinha <odamasenganumeko@gmail.com>
2023-10-01 17:33:28 -0300

    Banco atualizado com as novas procedures de cadastrar, atualizar e excluir Produtos

--- bancoBladeEnclave.sql
+++ bancoBladeEnclave.sql
@@ -25,6 +25,8 @@ sgLancamento enum('S','N') not null,
 constraint fk_Cat foreign key (cdCategoria) references tbCategoria(cdCategoria)
 
 )
+
+
 default character set utf8;
 
 -- inserindo dados na tabela de Categoria
@@ -43,7 +45,7 @@ insert into tbProdutos
 		 Sua empunhadura é longa o suficiente para acomodar duas mãos, o que a torna eficaz tanto para ataques cortantes quanto perfurantes
 		A Zweihänder era principalmente uma arma de guerra utilizada por soldados de infantaria, 
 		especialmente na Alemanha durante o século XVI. Devido ao seu tamanho e peso, era eficaz
-		 para desferir golpes poderosos em combate corpo a corpo. </p>", 'Zwei','N'),
+		 para desferir golpes poderosos em combate corpo a corpo. </p>", 'Zwei.jpg','N'),
        (default, 'Claymore', 7231.99, 2, 7,
 		"<p> A espada Claymore é uma espada histórica escocesa que se destacou durante o final da 
         Idade Média e o início da Era Moderna, especialmente nos séculos XVI e XVII. 
@@ -53,7 +55,7 @@ insert into tbProdutos
           Ela era uma arma versátil que podia ser usada tanto para ataques cortantes quanto para 
           perfurações. A característica mais notável da Claymore era o seu cabo longo, que permitia
           que a espada fosse empunhada com as duas mãos, proporcionando maior alcance e poder de corte. </p>", 
-          'Clay','S'),
+          'Clay.jpg','S'),
 
 -- Katanas
 	  (default, 'Koshi Sori O Kissaki', 4221.59, 1, 1,
@@ -62,14 +64,14 @@ insert into tbProdutos
        simples lâmina; é uma manifestação da tradição samurai em sua forma mais refinada.
 Lâmina Excepcional: A lâmina Koshi Sori O Kissaki é forjada com maestria por artesãos habilidosos, 
 combinando aço de alta qualidade com uma curvatura distinta e uma ponta aguçada e equilibrada para 
-precisão. </p>", 'Koshi','S'),
+precisão. </p>", 'Koshi.jpg','S'),
 	  (default, 'Gold Lion Wakizashi', 7212.50, 1, 4,
 	   "<p> Bem-vindo à nossa loja, onde a tradição samurai encontra a elegância atemporal. 
        Apresentamos a você o Gold Lion Wakizashi, uma obra-prima que incorpora a essência da cultura
        japonesa em uma lâmina única.
 Lâmina Lendária: A lâmina do Gold Lion Wakizashi é forjada com precisão, 
 usando métodos tradicionais transmitidos por gerações. Feita de aço de alta qualidade, 
-ela é afiada com maestria e decorada com gravuras que retratam a majestade de um leão dourado. </p>", 'Waki','N'),
+ela é afiada com maestria e decorada com gravuras que retratam a majestade de um leão dourado. </p>", 'Waki.jpg','N'),
 
 -- Adagas
   (default, 'Gan Jiang and Mo Ye', 4032.22, 3, 14,
@@ -82,13 +84,13 @@ A Gan Jiang é baseada em uma das duas lendárias espadas chinesas, Gan Jiang e 
  Mo Ye (Yin Sword): Mo Ye é a contrapartida de Gan Jiang, complementando sua natureza yang (fogo) com a natureza yin (água). Ela também é baseada na segunda espada lendária na mitologia chinesa.
 Mo Ye tem uma aparência que contrasta com a Gan Jiang, 
 sendo uma espada de lâmina larga e azulada.
- Ela reflete a dualidade de poder entre as duas espadas.</p>", 'fate','S'),
+ Ela reflete a dualidade de poder entre as duas espadas.</p>", 'fate.jpg','S'),
  
    (default, 'Adaga Javalizeira', 7212.50, 3, 4,
 	   "<p>A Adaga Javalizeira é uma criação singular que une a versatilidade
        de uma adaga com a potência de um arremesso de lança. Seu design inovador a torn
        a uma escolha excepcional para aqueles que buscam a combinação perfeita de alcance e
-       letalidade em uma única arma. </p>", 'Java','N'),
+       letalidade em uma única arma. </p>", 'Java.jpg','N'),
 
 -- Jian
 
@@ -97,7 +99,7 @@ sendo uma espada de lâmina larga e azulada.
        e resistente. O processo de forjamento é frequentemente uma demonstração de habilidade artesanal.
 Guarda-Mão Ornamentada: A guarda-mão da Jian é frequentemente adornada com detalhes
  intrincados e esculpidos à mão, representando temas da cultura chinesa, como dragões, 
- fênix ou símbolos de longevidade. </p>", 'Hsu','N'),
+ fênix ou símbolos de longevidade. </p>", 'Hsu.jpg','N'),
        
  (default, 'Taiji Bagua Jian', 4111.32, 4, 8,
 	   "<p>Taiji Bagua Jian é uma espada que se originou das tradições de Tai Chi e Baguazhang,
@@ -105,7 +107,7 @@ Guarda-Mão Ornamentada: A guarda-mão da Jian é frequentemente adornada com de
        equilíbrio, a fluidez e a conexão mente-corpo.
 Lâmina de Precisão: A espada Jian é reconhecida por sua lâmina longa, reta e fina, 
 que é projetada para cortes precisos e estocadas rápidas. A Taiji Bagua Jian incorpora
- movimentos graciosos que tiram o máximo proveito dessa lâmina. </p>", 'Tai','S');
+ movimentos graciosos que tiram o máximo proveito dessa lâmina. </p>", 'Tai.jpg','S');
 
  select * from tbProdutos;
  
@@ -126,22 +128,24 @@ where tbProdutos.cdCategoria = tbCategoria.cdCategoria;
  
 -- Procedure com a view com Inner Join
 
-CREATE  PROCEDURE listarProdutosDetalhes()
+CREATE  PROCEDURE listarProdutosDetalhes(vCod int)
 
 begin
  
- select * from VwProdDetalhes;
+ select * from VwProdDetalhes
+ where cdProd = vCod;
  
  end //
- 
+
  delimiter !!
 
 -- Procedure para Listar os Produtos na página index.php
+
  create procedure ListarProdutos()
  
  begin
  
-  select nmProd, vlProd, ImgProd, qtEstoque from tbProdutos
+  select cdProd, nmProd, vlProd, ImgProd, qtEstoque from tbProdutos
   order by nmProd asc;
  
  end !!
@@ -165,7 +169,7 @@ begin
  
  begin
  
-	select  nmProd,vlProd,ImgProd, qtEstoque from vwProdDetalhes where dsCategoria = vdsCategoria;
+	select  cdProd,nmProd,vlProd,ImgProd, qtEstoque from vwProdDetalhes where dsCategoria = vdsCategoria;
  
  end &&
  
@@ -247,3 +251,104 @@ begin
 
  end $$
  delimiter ;
+ 
+ -- criando procedure para listar produto buscado
+ 
+ delimiter %%
+ 
+ create procedure spListaBuscado(VnmProd varchar(70))
+
+ begin
+ 
+ select cdProd, ImgProd, nmProd, vlProd from tbProdutos where nmProd like concat ("%",'a', "%" );
+ 
+ end %%
+ 
+ delimiter ; 
+ 
+ -- criando procedure para listar as Categorias
+ 
+ delimiter &&
+ 
+ create procedure spListaCategoria()
+ 
+ begin
+ 
+ select * from tbCategoria;
+ 
+ end &&
+ 
+ delimiter ;
+ 
+ -- criando procedure para cadastrar Produtos
+ 
+ delimiter !!
+
+ create procedure inserirProduto(VnmProd varchar(70), VvlProd decimal(6,2), VcdCategoria int , VqtEstoque int, VResumo text, Vimg varchar(255), Vlanc enum('S','N') )
+ 
+ begin
+ 
+ insert into tbProdutos(nmProd, vlProd,cdCategoria,qtEstoque,ResumoProd,ImgProd,sgLancamento)
+ values(VnmProd, VvlProd, VcdCategoria, VqtEstoque, VResumo, Vimg,Vlanc);
+ 
+ end !!
+
+ delimiter ; 
+ 
+ -- criando procedure para selecionar produtos pelo código
+ 
+ delimiter %%
+ 
+ create procedure spSelecionaProdCod(vcdProd int)
+ 
+ begin
+ 
+ SELECT * FROM tbProdutos WHERE cdProd= vcdProd;
+ 
+ end %%
+ 
+
+ delimiter ; 
+ -- criando procedure de selecionar categoria pelo código
+ delimiter $$
+ 
+ create procedure spSelecionaCategoriaCod(vcdCategoria int, vcdCategoria2 int)
+ 
+ begin
+ 
+ select * from tbCategoria where cdCategoria = vcdCategoria
+ union select * from tbCategoria where cdCategoria != vcdCategoria2;
+ 
+ end $$
+ delimiter ; 
+ -- criando procedure para alterar produtos
+ 
+ delimiter %%
+
+ create procedure spAlteraProduto(VnmProd varchar(70), VvlProd decimal(6,2), VcdCategoria int, 
+                                 VqtEstoque int, VresumoProd text, vImgProd varchar(255), 
+								VsgLancamento enum('S', 'N'), VcdProd int)
+ 
+ begin
+ 
+ update tbProdutos set nmProd = VnmProd, vlProd = VvlProd, cdCategoria =  VcdCategoria, qtEstoque = VqtEstoque,
+ ResumoProd =  VresumoProd, ImgProd = vImgProd, sgLancamento = VsgLancamento where cdProd =  VcdProd;
+ 
+ end %%
+ 
+ delimiter ; 
+ -- criando procedure para excluir Produto
+ 
+ delimiter %%
+ 
+ create procedure spExcluiProduto(vCod int)
+ 
+ begin
+ 
+ delete  from tbProdutos where cdProd = vCod;
+ 
+ end %%
+ 
+ delimiter ; 
+ 
+ 
